fbbe7c3e45687fb7e01e10251ea1f19dce36fe58
SJ <seung-jae.bang@spglobal.com>
2019-05-12 22:21:39 -0400

    fix bug

--- partition_data.sh
+++ partition_data.sh
@@ -1,3 +1,5 @@
-python prepare_data_run.py \
+python partition_data_run.py \
     --raw_source_dir /Users/seung-jae_bang/Personal/Columbia/Applied_DL/Camelyon_Project/data/source_data \
-    --meta_data_dir /Users/seung-jae_bang/Personal/Columbia/Applied_DL/Camelyon_Project/data/test_dir
+    --meta_data_dir /Users/seung-jae_bang/Personal/Columbia/Applied_DL/Camelyon_Project/data/test_dir \
+    --img_data_dir /Users/seung-jae_bang/Personal/Columbia/Applied_DL/Camelyon_Project/data/test_dir/input_data \
+    --img_partition_option zoom_1_256_256
